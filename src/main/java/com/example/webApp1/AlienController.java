7b7f199a13d53ff02206e0e709fba327a184131f
Bhavesh0510 <bhaveshshelar0510@gmail.com>
2022-01-18 16:12:23 +0530

    updated rest

--- src/main/java/com/example/webApp1/AlienController.java
+++ src/main/java/com/example/webApp1/AlienController.java
@@ -1,9 +1,22 @@
 package com.example.webApp1;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.plaf.OptionPaneUI;
+
+import org.hibernate.query.criteria.internal.expression.function.AggregationFunction.LEAST;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,8 +39,37 @@ public class AlienController {
     public ModelAndView getAlien(@RequestParam int aid) {
         ModelAndView mv = new ModelAndView("search.jsp");
         Alien alien = repo.findById(aid).orElse(new Alien());
+
+        System.out.println(repo.findByAtech("Java"));
+        System.out.println(repo.findByAidGreaterThan(102));
+        System.out.println(repo.findByAtechSorted("Java"));
+
         mv.addObject(alien);
         return mv;
+    }
+
+    // @RequestMapping("/aliens")
+    // @ResponseBody
+    // public String getAliens() {
+    // return repo.findAll().toString();
+    // }
+
+    // @RequestMapping("/alien/{aid}")
+    // @ResponseBody
+    // public String getAlien2(@PathVariable("aid") int aid) {
+    // return repo.findById(aid).toString();
+    // }
+
+    @GetMapping("/aliens")
+    @ResponseBody
+    public List<Alien> getAliens() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/alien/{aid}")
+    @ResponseBody
+    public Optional<Alien> getAlien2(@PathVariable("aid") int aid) {
+        return repo.findById(aid);
     }
 
     @RequestMapping("/delAlien")
@@ -35,6 +77,25 @@ public class AlienController {
         ModelAndView mv = new ModelAndView("delete.jsp");
         repo.deleteById(aid);
         return mv;
+    }
+
+    @DeleteMapping("/alien/{aid}")
+    public String deleteAlien(@PathVariable int aid) {
+        Alien a = repo.getById(aid);
+        repo.delete(a);
+        return "Deleted";
+    }
+
+    @PostMapping(path = "/alien", consumes = { "application/json" })
+    public Alien addAlien2(@RequestBody Alien alien) {
+        repo.save(alien);
+        return alien;
+    }
+
+    @PutMapping(path = "/alien", consumes = { "application/json" })
+    public Alien saveOrupdateAlien2(@RequestBody Alien alien) {
+        repo.save(alien);
+        return alien;
     }
 
     @RequestMapping("/delAlien2")
--- src/main/java/com/example/webApp1/AlienRepo.java
+++ src/main/java/com/example/webApp1/AlienRepo.java
@@ -1,7 +1,18 @@
 package com.example.webApp1;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AlienRepo extends CrudRepository<Alien,Integer> {
+import java.util.List;
+
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+
+    List<Alien> findByAtech(String tech);
+
+    List<Alien> findByAidGreaterThan(int aid);
+
+    @Query("from Alien where atech=?1 order by aname")
+    List<Alien> findByAtechSorted(String tech);
 
 }
